69c066ea85776845dc75cf848d6bb22d1a33454c
tsyamagu <tsyamagu@C010423674.zipangu.cac.co.jp>
2026-04-15 10:57:22 +0900

    あ

--- Java100_questions_cac2nd/src/lesson01/challenge01/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge01/Patisserie.java
@@ -16,7 +16,7 @@ package lesson01.challenge01;
 
 public class Patisserie {
 	public static void main(String[] args) {
-		System.out.println("たいへんお待たせしました。");
+		System.out.println("たいへんお待たせしました。　");
 		System.out.println("【ポエール・ネルメ】");
 		System.out.println("ただいまより開店です！！");
 
