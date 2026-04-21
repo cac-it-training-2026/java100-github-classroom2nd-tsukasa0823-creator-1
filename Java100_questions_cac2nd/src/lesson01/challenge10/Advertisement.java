4ccdc17d14470ae27b1a5260917aaf3b984e6bf8
tsyamagu <tsyamagu@C010423674.zipangu.cac.co.jp>
2026-04-21 16:25:21 +0900

    lesson01のchallenge10完了

---
+++ Java100_questions_cac2nd/src/lesson01/challenge10/Advertisement.java
@@ -0,0 +1,25 @@
+package lesson01.challenge10;
+
+public class Advertisement {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("ホール、キッチンを募集してます！");
+		System.out.println("9時から17時半");
+		System.out.println("時給10000円");
+		System.out.println("賄い可能！！！");
+
+	}
+
+}
+
+//
+//処理の内容：募集要項の表示(以下の項目を表示し、
+//                           詳細及び項目の追加等は自由とする)
+//
+//            ①店員募集のメッセージ
+//            ②勤務時間
+//            ③時給
+//            ④待遇（制服の貸与など）
+//
+//
