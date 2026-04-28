0aa5f41b93e22ad8843695ac571e5ec56e12019e
tsyamagu <tsyamagu@C010423674.zipangu.cac.co.jp>
2026-04-28 17:00:11 +0900

    lesson05の1から7まで完了

--- Java100_questions_cac2nd/src/lesson05/challenge05/RobotMaker.java
+++ Java100_questions_cac2nd/src/lesson05/challenge05/RobotMaker.java
@@ -34,32 +34,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 //ここに問題4で作成したクラスに次の条件を足したクラスを作成してください。
+class Robot {
+	int energy;
+	String name;
+	int water;
+
+	void setWater(int water) {
+		this.water = water;
+
+	}
+
+	void pumpWater() {
+		System.out.println("\n水を" + water + "リットル出します。\n");
+
+	}
+
+	void randomWater() {
+		water = (int) (Math.random() * 9) + 1;
+	}
+}
 //メソッド名：setWater(引数int water、戻り値void、フィールドwaterに引数の値を代入する)
 //メソッドpumpWaterを改修し、randomWater()を削除
 
 public class RobotMaker {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        System.out.println("Rさん：");
-        System.out.println("でもランダムだと必要な量だけ出せないですね。\n");
-        System.out.println("G博士：");
-        System.out.println("そう言うと思って水量調節機能を実装してみたぞ。\n");
-        System.out.println("Rさん：");
-        System.out.println("どうやって使うんですか？\n");
-        System.out.println("G博士：");
-        System.out.println("簡単じゃ。必要な水量をリットル単位で入力すればいいんじゃ。\n");
+		System.out.println("Rさん：");
+		System.out.println("でもランダムだと必要な量だけ出せないですね。\n");
+		System.out.println("G博士：");
+		System.out.println("そう言うと思って水量調節機能を実装してみたぞ。\n");
+		System.out.println("Rさん：");
+		System.out.println("どうやって使うんですか？\n");
+		System.out.println("G博士：");
+		System.out.println("簡単じゃ。必要な水量をリットル単位で入力すればいいんじゃ。\n");
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("出したい水の量を入力してください＞");
-        String waterStr = br.readLine();
-        int water = Integer.parseInt(waterStr);
+		System.out.print("出したい水の量を入力してください＞");
+		String waterStr = br.readLine();
+		int water = Integer.parseInt(waterStr);
 
-        //ここでRobotクラスのインスタンスを作り、
-        //（インスタンス名はrobot）
-        //setWaterを実行する。
-        //pumpWaterを実行する。
-    }
+		//ここでRobotクラスのインスタンスを作り、
+		Robot robot = new Robot();
+		//（インスタンス名はrobot）
+		//setWaterを実行する。
+		robot.setWater(water);
+		//pumpWaterを実行する。
+		robot.pumpWater();
+	}
 
 }
